4554a598b14c3e8fe12d73760d257fc34f2f11c7
Bibek Tamang <bibek7here@gmail.com>
2025-04-28 17:47:29 +0545

    completed trpc logic

--- apps/web/trpc/router.ts
+++ apps/web/trpc/router.ts
@@ -2,8 +2,12 @@ import type { TRPCRouterRecord } from "@trpc/server";
 import { z } from "zod";
 import { createTrpcRouter, publicProcedure } from "./init";
 import { S3 } from "@/lib/r2";
+import { prismaClient } from "db";
+import { addVideoSummary, addVideoTranscribe } from "queue";
 
 let writer: Bun.NetworkSink | null = null;
+
+const R2_BUCKET = process.env.NEXT_PUBLIC_R2_BUCKET!;
 
 const uploadChunk = {
 	initialize: publicProcedure
@@ -25,15 +29,43 @@ const uploadChunk = {
 		.input(
 			z.object({
 				file: z.any(), //for now
-				fheel: z.any(),
 			})
 		)
 		.mutation(({ input }) => {
 			writer?.write(input.file);
 		}),
-	endUpload: publicProcedure.mutation(() => {
-		writer?.end();
-	}),
+	endUpload: publicProcedure
+		.input(
+			z.object({
+				videoId: z.string(),
+			})
+		)
+		.mutation(async ({ input }) => {
+			await writer?.end();
+			try {
+				const video = await prismaClient.video.findUniqueAndUpdate({
+					where: {
+						id: input.videoId,
+					},
+					data: {
+						url: `${R2_BUCKET}/${input.videoId}/mp4`,
+					},
+				});
+
+				if (!video) {
+					return false;
+				}
+				await addVideoTranscribe({
+					videoId: video.id,
+					videoUrl: `${R2_BUCKET}/${video.id}/mp4`,
+				});
+				await addVideoSummary({ summaryType: "short", videoId: video.id });
+				return true;
+			} catch (error: any) {
+				console.log("something went wrong", error);
+				return false;
+			}
+		}),
 } satisfies TRPCRouterRecord;
 
 export const trpcRouter = createTrpcRouter({
