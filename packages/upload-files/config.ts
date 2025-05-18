a45efca67e2cfb049d9bb29c69539812e045567f
Bibek Tamang <bibek7here@gmail.com>
2025-05-18 23:19:30 +0545

    fixing upload files config

--- packages/upload-files/config.ts
+++ packages/upload-files/config.ts
@@ -3,9 +3,11 @@ import { S3Client } from "@aws-sdk/client-s3";
 const R2_ACCESS_KEY_ID = process.env.R2_ACCESS_KEY_ID!;
 const R2_SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY!;
 // const R2_BUCKET = process.env.R2_BUCKET!;
+console.log(R2_ACCESS_KEY_ID, "thisis access key id")
 
+console.log(R2_SECRET_ACCESS_KEY, "thisis secret accessj key")
 export const s3 = new S3Client({
-	region: "auto",
+	region: "us-east-1",
 	// endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
 	endpoint: "http://localhost:9000",
 	forcePathStyle: true,
@@ -13,5 +15,5 @@ export const s3 = new S3Client({
 		accessKeyId: R2_ACCESS_KEY_ID,
 		secretAccessKey: R2_SECRET_ACCESS_KEY,
 	},
-	bucketEndpoint: true,
+	// bucketEndpoint: true,
 });
--- packages/upload-files/index.ts
+++ packages/upload-files/index.ts
@@ -3,16 +3,21 @@ import { s3 } from "./config";
 import {
 	CompleteMultipartUploadCommand,
 	CreateMultipartUploadCommand,
+	ListBucketsCommand,
 	UploadPartCommand,
 } from "@aws-sdk/client-s3";
 
 const R2_BUCKET = process.env.R2_BUCKET!;
 
 export class StreamUploadFile {
-	private uploadId: string | undefined = undefined;
+	public uploadId: string | undefined = undefined;
 	private eTag: string | undefined = undefined;
-	private key: string | null = null;
+	public key: string | null = null;
 	private storedParts: Array<{ ETag: string; PartNumber: number }> = [];
+	async check() {
+		const result = await s3.send(new ListBucketsCommand());
+		console.log("Buckets:", result.Buckets);
+	}
 	public async start(fileName: string) {
 		const command = new CreateMultipartUploadCommand({
 			Bucket: R2_BUCKET,
@@ -23,31 +28,23 @@ export class StreamUploadFile {
 		this.uploadId = UploadId;
 		this.key = fileName;
 	}
-	//TODO: comes blob from frontend
-	public async upload(data: Blob, partNumber: number) {
+	public async upload(data: Buffer<ArrayBuffer>, partNumber: number) {
 		if (!this.uploadId || !this.key) {
 			return;
 		}
 		try {
-			const command = new UploadPartCommand({
-				Bucket: R2_BUCKET,
-				Key: this.key,
-				PartNumber: partNumber,
-				Body: data,
-				UploadId: this.uploadId,
-			});
-
 			const uploadRes = await this.uploadPartWithRetry(
 				{
 					Bucket: R2_BUCKET,
 					Key: this.key,
 					PartNumber: partNumber,
-					Body: data,
+					Body: Buffer.from(data),
 					UploadId: this.uploadId,
 				},
 				3
 			);
 			if (!uploadRes) {
+				console.log("what went wrong");
 				return;
 			}
 
@@ -62,6 +59,7 @@ export class StreamUploadFile {
 		if (!this.eTag || !this.key) {
 			return;
 		}
+		console.log("this is upload id in upload stop", this.uploadId);
 		const command = new CompleteMultipartUploadCommand({
 			Bucket: R2_BUCKET,
 			Key: this.key,
@@ -83,15 +81,18 @@ export class StreamUploadFile {
 		maxRetries: number = 3
 	) {
 		let attempt = 0;
+		const command = new UploadPartCommand(params);
 		while (attempt < maxRetries) {
 			try {
-				const result = await s3.send(new UploadPartCommand(params));
+				const result = await s3.send(command);
 				return result;
 			} catch (error) {
 				attempt++;
-				console.warn(`upload part ${params.PartNumber} failed`);
-				if (attempt >= maxRetries)
+				console.warn(`upload part ${params.PartNumber} failed`, error);
+				if (attempt >= maxRetries) {
+					console.log("this is error ", error);
 					throw new Error(`Failed to upload part ${params.PartNumber}`);
+				}
 
 				await new Promise((res) => setTimeout(res, 2 ** attempt * 100));
 			}
