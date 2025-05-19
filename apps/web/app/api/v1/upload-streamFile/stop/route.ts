bf5d94aa66ce999a3080a2fc189caded6a14dd25
Bibek Tamang <bibek7here@gmail.com>
2025-05-19 23:01:00 +0545

    update config

--- apps/web/app/api/v1/upload-streamFile/stop/route.ts
+++ apps/web/app/api/v1/upload-streamFile/stop/route.ts
@@ -1,11 +1,12 @@
 import type { NextRequest } from "next/server";
-import { plainToInstance } from "class-transformer";
-import { redisClient } from "redis-config";
+// import { plainToInstance } from "class-transformer";
+// import { redisClient } from "redis-config";
 import { NextResponse } from "next/server";
-import { StreamUploadFile } from "upload-files";
+// import { StreamUploadFile } from "upload-files";
 import { recordStop } from "queue";
 
 export async function PATCH(req: NextRequest) {
+	console.log("this should hit atleast once or max thrice")
 	const uploadId = req.headers.get("x-upload-id");
 	if (!uploadId) {
 		return NextResponse.json({ success: false }, { status: 400 });
--- apps/web/services/bot-spawner.ts
+++ apps/web/services/bot-spawner.ts
@@ -1,58 +1,57 @@
-import { spawn } from "child_process";
-import { prismaClient } from "db";
+// import { spawn } from "child_process";
+import { recordMeeting } from "queue";
 
-export const assignBotToMeeting = (videoId: string, meetingURL: string) => {
-	const containerName = `bot-${videoId}`;
-	const backendURL = process.env.AUTH_URL! || "http://localhost:3000";
-
-	const dockerArgs = [
-		"run",
-		"-d",
-		"--rm",
-		"-e",
-		backendURL,
-		"--name",
-		containerName,
-		"recording-bot",
-		videoId,
-		meetingURL,
-	];
-
-	const dockerProcess = spawn("docker", dockerArgs);
-	dockerProcess.stdout.on("data", (data) => {
-		console.log("container name", data);
-	});
-
-	dockerProcess.stderr.on("data", async (data) => {
-		console.error("container name error", data.toString());
-		await updateWithRetry(videoId);
-	});
-
-	dockerProcess.on("close", (code) => {
-		console.log("Container exited with code ", code);
-	});
+export const assignBotToMeeting = async (videoId: string, meetingUrl: string) => {
+	console.log("yeac chhai aayo hai")
+	await recordMeeting({ videoId, meetingUrl });
+	//TODO: This is production ready setup
+	// const containerName = `bot-${videoId}`;
+	// const backendURL = process.env.AUTH_URL! || "http://localhost:3000";
+	// const dockerArgs = [
+	// 	"run",
+	// 	"-d",
+	// 	"--rm",
+	// 	"-e",
+	// 	backendURL,
+	// 	"--name",
+	// 	containerName,
+	// 	"recording-bot",
+	// 	videoId,
+	// 	meetingURL,
+	// ];
+	// const dockerProcess = spawn("docker", dockerArgs);
+	// dockerProcess.stdout.on("data", (data) => {
+	// 	console.log("container name", data);
+	// });
+	// dockerProcess.stderr.on("data", async (data) => {
+	// 	console.error("container name error", data.toString());
+	// 	await updateWithRetry(videoId);
+	// });
+	// dockerProcess.on("close", (code) => {
+	// 	console.log("Container exited with code ", code);
+	// });
 };
 
-function updateWithRetry(videoId: string) {
-	return new Promise(async (resolve, reject) => {
-		let retry = 0;
-		while (retry !== 3) {
-			try {
-				setTimeout(async () => {
-					await prismaClient.video.update({
-						where: {
-							id: videoId,
-						},
-						data: {
-							recordingBotStatus: "FAILED",
-						},
-					});
-					return resolve("Updated successfully.");
-				}, 15000);
-			} catch (error) {
-				if (retry == 2) return reject("Failed to updated status.");
-				retry++;
-			}
-		}
-	});
-}
+// function updateWithRetry(videoId: string) {
+// 	return new Promise(async (resolve, reject) => {
+// 		let retry = 0;
+// 		while (retry !== 3) {
+// 			try {
+// 				setTimeout(async () => {
+// 					await prismaClient.video.update({
+// 						where: {
+// 							id: videoId,
+// 						},
+// 						data: {
+// 							recordingBotStatus: "FAILED",
+// 						},
+// 					});
+// 					return resolve("Updated successfully.");
+// 				}, 15000);
+// 			} catch (error) {
+// 				if (retry == 2) return reject("Failed to updated status.");
+// 				retry++;
+// 			}
+// 		}
+// 	});
+// }
